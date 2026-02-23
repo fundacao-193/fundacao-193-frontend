d3e8c51911eb4a3d3b852334eccedc2836d9d18d
Natan Antonucci <natanantonucci15@gmail.com>
2026-02-23 16:28:55 -0300

    Add minimal worker.js for Cloudflare static site serving

---
+++ worker.js
@@ -0,0 +1,10 @@
+/**
+ * Minimal Worker entry point
+ * Wrangler will serve static files from [site] bucket automatically
+ */
+
+export default {
+  fetch() {
+    // Static files are served by Wrangler's built-in middleware
+  },
+};
